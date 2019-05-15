5faff05e9d912e049222adaf5d7f34bca15aa197
Matt Novnestern <fisxoj@gmail.com>
2019-05-14 21:03:04 -0400

    Return an alist

--- example/hello.lisp
+++ example/hello.lisp
@@ -7,9 +7,6 @@
 (defun hello (event)
   (let ((name (cdr (assoc "name" event :test #'string=))))
     (if name
-	(with-output-to-string (s)
-          (jonathan.encode:with-output (s)
-            (jonathan:with-object
-              (jojo:write-key-value "response" (format nil "Hello, ~a!" name))
-              (jojo:write-key-value "request-id" (cl-aws-lambda/runtime-interface:request-id-of cl-aws-lambda/runtime-interface:*context*)))))
+        (list (cons "response" (format nil "Hello, ~a!" name))
+              (cons "resquest-id" (cl-aws-lambda/runtime-interface:request-id-of cl-aws-lambda/runtime-interface:*context*)))
 	(error "No name supplied!"))))
