50a8335aae0e548f7db4790bb0fb1e96784f7119
Matt Novenstern <fisxoj@gmail.com>
2020-08-30 11:25:15 -0400

    Fix build path to be a directory

--- example/build.lisp
+++ example/build.lisp
@@ -1,7 +1,7 @@
 #!/usr/local/bin/sbcl --script
 (load "~/quicklisp/setup")
 
-(push "/work" asdf:*central-registry*)
+(push "/work/" asdf:*central-registry*)
 
 ;; #+sb-core-compression
 ;; (defmethod asdf:perform ((o asdf/bundle:program-op) (c asdf:system))
