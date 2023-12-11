0ca349fdf54f8f6f8128a2da457005237cf3b0d4
Markus Klink <justjoheinz@gmail.com>
2023-12-11 14:56:55 +0100

    More changes

--- example/hello.lisp
+++ example/hello.lisp
@@ -4,9 +4,11 @@
 
 (in-package :example-lambda)
 
+(declaim (optimize (space 3) (speed 0)))
+
 (defun hello (event)
   (let ((name (cdr (assoc "name" event :test #'string=))))
     (if name
         (list (cons "response" (format nil "Hello, ~a!" name))
-              (cons "resquest-id" (cl-aws-lambda/runtime-interface:request-id-of cl-aws-lambda/runtime-interface:*context*)))
-	(error "No name supplied!"))))
+              (cons "request-id" (cl-aws-lambda/runtime-interface:request-id-of cl-aws-lambda/runtime-interface:*context*)))
+        (error "No name supplied!"))))
